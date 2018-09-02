271d081b26dc6deb76de8625eef469d6e46b153b
Clarinda Bui <clarindabui@outlook.com>
2018-09-02 12:40:46 -0700

    Add meta tags

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiY2J1aSIsImEiOiJjamxsNmh0azkwdTd1M3VsZHY2Y2V0ZmV1In0.1zsMcc7BC3kf3Hq_1RYKeA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
